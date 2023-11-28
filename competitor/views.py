2216a96689fadccdd74f268fcce915a6e7f6b2a0
adrianhrb <adrian.herrera.br@gmail.com>
2023-11-28 12:06:00 +0000

    Some changes on competitors

--- competitor/views.py
+++ competitor/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Competitor
+
+
+def competitor_list(request):
+    competitors_active = Competitor.objects.filter(active=True)
+    eliminated_competitors = Competitor.objects.filter(active=False)
+    return render(
+        request,
+        'list.html',
+        dict(
+            competitors_active=competitors_active,
+            eliminated_competitors=eliminated_competitors,
+            section='concursantes',
+        ),
+    )
