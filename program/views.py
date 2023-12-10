be7f78c5fc9cc28c3a5845b0370afecef0c0acd3
adrianhrb <adrian.herrera.br@gmail.com>
2023-12-10 13:31:47 +0000

    Search results done

--- program/views.py
+++ program/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from competitor.models import Competitor
+from teacher.models import Teacher
+from judge.models import Judge
 
 
 def index(request):
     return render(request, 'index.html')
 
 def search(request, tofind: str):
-    competitors_title = Competitor.objects.filter(first_name__icontains = tofind)
+    competitors = Competitor.objects.filter(first_name__icontains = tofind)
     music_styles = Competitor.objects.filter(music_style__name__icontains = tofind)
-    return render(request, 'results.html', dict(competitors_title=competitors_title, music_styles=music_styles))
+    teachers = Teacher.objects.filter(first_name__icontains=tofind)
+    judges = Judge.objects.filter(first_name__icontains = tofind)
+    return render(request, 'results.html', dict(competitors=competitors, music_styles=music_styles, teachers=teachers, judges=judges))
