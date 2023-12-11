faaae4aae85726bf35ca624fc7d279e8e19f0278
adrianhrb <adrian.herrera.br@gmail.com>
2023-12-11 11:02:26 +0000

    Changes to find for last and first name

--- program/views.py
+++ program/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from competitor.models import Competitor
 from teacher.models import Teacher
 from judge.models import Judge
+from django.db.models import Q
 
 
 def index(request):
     return render(request, 'index.html')
 
 def search(request, tofind: str):
-    competitors = Competitor.objects.filter(first_name__icontains = tofind)
-    music_styles = Competitor.objects.filter(music_style__name__icontains = tofind)
+    competitors = Competitor.objects.filter(Q(first_name__icontains=tofind) | Q(last_name__icontains=tofind))
+    music_styles = Competitor.objects.filter(music_style__name__icontains=tofind)
     teachers = Teacher.objects.filter(first_name__icontains=tofind)
     judges = Judge.objects.filter(first_name__icontains = tofind)
     return render(request, 'results.html', dict(competitors=competitors, music_styles=music_styles, teachers=teachers, judges=judges))
