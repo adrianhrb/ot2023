78dbe2de1eed0f3dab7b11c4520926269e271aa6
adrianhrb <adrian.herrera.br@gmail.com>
2024-02-20 18:41:47 +0000

    Some changes on poll views and vote template

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from competitor.models import Competitor
+
+
+def saving_competitor(request):
+    """
+    Saving not as a database concept, but as saving the competitor for beeing eliminated
+    """
+    nominated_competitors = Competitor.objects.filter(nominated=True)
+    return render(request, 'vote/save.html', dict(nominated=nominated_competitors))
